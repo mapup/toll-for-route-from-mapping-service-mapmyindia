0157f6edd4e44e2c92589a9d744d321f37c6aa30
saransh-mapup <77100515+saransh-mapup@users.noreply.github.com>
2021-01-18 23:36:19 +0530

    Update Program.cs

--- .net/Program.cs
+++ .net/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using RestSharp;
 
 namespace mapmyIndia
 {
@@ -17,16 +18,8 @@ namespace mapmyIndia
             string url="https://apis.mapmyindia.com/advancedmaps/v1/"+api_key+"/route_adv/driving/"+source_longitude+","+source_latitude+";"+destination_longitude+","+destination_latitude+"?steps=false&rtype=1";
             Console.WriteLine(url);
             WebRequest request = WebRequest.Create(url);
-            //WebRequest request = WebRequest.Create(
-            //  "https://apis.mapmyindia.com/advancedmaps/v1/api_key/route_adv/driving/80.131123,28.552413;77.113091,28.544649?steps=false&rtype=1");
-            // If required by the server, set the credentials.
-            // Get the response.
             WebResponse response = request.GetResponse();
-            // Display the status.
-            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
             String polyline;
-            // Get the stream containing content returned by the server.
-            // The using block ensures the stream is automatically closed.           
             using (Stream dataStream = response.GetResponseStream())
             {
                 // Open the stream using a StreamReader for easy access.
@@ -38,11 +31,21 @@ namespace mapmyIndia
                 string[] temp = output[3].Split("\"");
                 polyline=temp[0];
             }
-            Console.WriteLine(polyline);
-            
-
             // Close the response.
             response.Close();
+
+    /***********Toll Guru API*****************/
+            var client = new RestClient("https://dev.tollguru.com/v1/calc/route");
+            var request1 = new RestRequest(Method.POST);
+            request1.AddHeader("content-type", "application/json");
+            request1.AddHeader("x-api-key", "");
+            request1.AddParameter("application/json", "{\"source\":\"mapmyindia\" , \"polyline\":\""+polyline+"\" }", ParameterType.RequestBody);
+            IRestResponse response1 = client.Execute(request1);        
+            var content = response1.Content;
+            string[] result = content.Split("tag\":");
+            string[] temp1 = result[1].Split(",");
+            string cost = temp1[0];
+            Console.WriteLine(cost);
         }
     }
 }
